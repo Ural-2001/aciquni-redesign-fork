a0ee9cbfca5e7208995d3b1747149622afa1cbca
Ural <ural2001@gmail.com>
2022-09-07 17:05:17 +0300

    add register template

--- src/lib/popups/login/Login.svelte
+++ src/lib/popups/login/Login.svelte
@@ -16,42 +16,109 @@
 </script>
 
 <div class="popup" id="loginPopup">
-    <div>
-        <div class="login-header">
-            <div class="login-links">  
-                <a href="">Керү</a>
-                <a href="" style="color: #CBCBCB;">Теркәү</a>
+    {#if false}
+        <div>
+            <div class="login-header">
+                <div class="login-links">  
+                    <a href="">Керү</a>
+                    <a href="" style="color: #CBCBCB;">Теркәү</a>
+                </div>
             </div>
+            <form class="login-form">
+                <div class="form-input">
+                    <div class="form-input-field">
+                        <label for="email">Электрон почта</label>
+                        <input type="text" id="email" placeholder="name@mail.com">
+                    </div>
+                    <img src="./icons/user.svg" alt="">
+                </div>
+                <div class="form-input">
+                    <div class="form-input-field">
+                        <label for="password">Сезнең пароль</label>
+                        <input type="text" id="password" placeholder="****">
+                    </div>
+                    <img src="./icons/lockOpen.svg" alt="">
+                </div>
+                <div class="popup-bottom">
+                    <button type="submit" class="button submit-button">
+                        Керү
+                        <img src="./icons/ArrowUpRightWhite.svg" alt="">
+                    </button>
+                    <a href="#" class="forgot-password">Пароль онытылдымы?</a>
+                </div>
+            </form>
+        </div> 
+        <div>
+            <button class="button-secondary close-button" on:click={closePopup}>
+                <img src="./icons/x.svg" alt="">
+            </button>
         </div>
-        <form class="login-form">
-            <div class="form-input">
-                <div class="form-input-field">
-                    <label for="email">Электрон почта</label>
-                    <input type="text" id="email" placeholder="name@mail.com">
+    {:else}
+        <div>
+            <div class="login-header">
+                <div class="login-links">  
+                    <a href="">Керү</a>
+                    <a href="" style="color: #CBCBCB;">Теркәү</a>
                 </div>
-                <img src="./icons/user.svg" alt="">
             </div>
-            <div class="form-input">
-                <div class="form-input-field">
-                    <label for="password">Сезнең пароль</label>
-                    <input type="text" id="password">
+            <form class="login-form">
+                <div class="form-input">
+                    <div class="form-input-field">
+                        <label for="name">Исемегез</label>
+                        <input type="text" id="name" value="Александр">
+                    </div>
                 </div>
-                <img src="./icons/lockOpen.svg" alt="">
-            </div>
-            <div class="popup-bottom">
-                <button type="submit" class="button submit-button">
-                    Керү
-                    <img src="./icons/ArrowUpRightWhite.svg" alt="">
-                </button>
-                <a href="#" class="forgot-password">Пароль онытылдымы?</a>
-            </div>
-        </form>
-    </div>
-    <div>
-        <button class="button-secondary close-button" on:click={closePopup}>
-            <img src="./icons/x.svg" alt="">
-        </button>
-    </div>
+                <div class="form-input">
+                    <div class="form-input-field">
+                        <label for="phone">Телефон номеры</label>
+                        <input type="tel" id="phone" value="+7 999 156-19-92">
+                    </div>
+                </div>
+                <div class="form-input">
+                    <div class="form-input-field">
+                        <label for="email">Электрон почта</label>
+                        <input type="email" id="email" value="alexkama@mail.com">
+                    </div>
+                </div>
+                <div class="form-input">
+                    <div class="form-input-field">
+                        <label for="password">Туган көнегез</label>
+                        <input type="date" id="password" value="26.10.1904">
+                    </div>
+                    <img src="./icons/Calendar.svg" alt="">
+                </div>
+                <div class="form-input">
+                    <div class="form-input-field">
+                        <label for="password1">Пароль уйлап табыгыз</label>
+                        <input type="password" id="password" placeholder="****">
+                    </div>
+                    <img src="./icons/Eye.svg" alt="">
+                </div>
+                <div class="form-input">
+                    <div class="form-input-field">
+                        <label for="password2">Парольне кабатлагыз</label>
+                        <input type="password" id="password2" placeholder="****">
+                    </div>
+                    <img src="./icons/EyeSlash.svg" alt="">
+                </div>
+                <div class="check-terms">
+                    <input type="checkbox" id="check-terms">
+                    <label for="check-terms">Принимаю условия обработки пользовательских данных</label>
+                </div>
+                <div class="popup-bottom">
+                    <button type="submit" class="button submit-button">
+                        Теркәү
+                        <img src="./icons/ArrowUpRightWhite.svg" alt="">
+                    </button>
+                </div>
+            </form>
+        </div> 
+        <div>
+            <button class="button-secondary close-button" on:click={closePopup}>
+                <img src="./icons/x.svg" alt="">
+            </button>
+        </div>
+    {/if}
 </div>
 
 <style>
@@ -141,5 +208,16 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
+    }
+
+    .check-terms {
+        display: flex;
+        font-size: 11px;
+        color: #B4B4B4;
+        margin-bottom: 30px;
+        margin-top: 30px;
+    }
+    .check-terms input{
+        margin-right: 10px;
     }
 </style>
