053459443a206cabacd1c6595e298cc4531201ca
Ural <ural2001@gmail.com>
2022-10-21 15:46:04 +0300

    make footer search area

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,107 +1,90 @@
-<script>
-	import Navbar from '$lib/navbar/Navbar.svelte';
-</script>
-
-<header class="container">
-	<div class="left-links">
-		<a href="#" class="button-header">
-			Тарих
-		</a>
-		<a href="#" class="button-header">
-			Мәдәният
-		</a>
-		<a href="#" class="button-header">
-			Дизайн
-		</a>
-		<a href="#" class="button-header">
-			Икътисад
-		</a>
-		<a href="#" class="button-header">
-			Әдәбият
-		</a>
-		<a href="#" class="button-secondary">
-			<span>Барча</span>
-			<img src="./icons/ArrowUpRight.svg" alt="">
-		</a>
+<div class="search-section">
+	<div class="search-block">
+		<div class="search">
+			<input type="text" placeholder="PR һәм реклам, дизайн, икътисад...">
+		</div>
+		<div class="search-block-actions">
+			<div class="social-buttons">
+				<a href="" class="social">
+					<img src="./icons/social/whatsapp.svg" alt="">
+				</a>
+				<a href="" class="social">
+					<img src="./icons/social/telegram.svg" alt="">
+				</a>
+				<a href="" class="social">
+					<img src="./icons/social/youtube.svg" alt="">
+				</a>
+				<a href="" class="social">
+					<img src="./icons/social/vk.svg" alt="">
+				</a>
+			</div>
+			<a href="/" class="new-courses-button">
+				Яңа онлайн курслар
+				<img src="./icons/ArrowUpRightWhite.svg" alt="">
+			</a>	
+		</div>
 	</div>
+</div>
 
-	<a href="">
-		<img src="./aciquni-logo.svg" alt="">
-	</a>
+<footer class="container">
 
-	<div class="right-links">
-		<a href="">
-			Сайланма
-		</a>
-		<a href="">
-			Фикерләр
-		</a>
-		<a href="">
-			Элемтә
-		</a>
-		<a href="" style="display:flex; align-items:center; gap: 5px;">
-			Тат
-			<img src="./icons/globe.svg" alt="">
-		</a>
-	</div>
+</footer>
 
-</header>
-
-<Navbar />
-
-<style type="text/scss">
-	header {
-		padding-top: 45px;
+<style>
+	.search-section {
+		margin-top: 100px;
+		padding-right: 192px;
+	}
+	.search-block {
+		border: 1px solid #1A233E;
+		border-left: 0;
+		padding: 28px;
+		border-radius: 0 100px 100px 0;
+		padding-left: 192px;
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		font-size: 12px;
 	}
-	.left-links {
+	.search input {
+		border: none;
+		color: #1A233E;
+		font-size: 20px;
+		width: 360px;
+		background: url(./MagnifyingGlass.svg) no-repeat scroll 0 50%;
+		padding-left: 30px;
+	}
+	.search input::placeholder {
+		color: #B9B9B9;
+	}
+	.search-block-actions {
 		display: flex;
-		flex-wrap: wrap;
+	}
+	.social-buttons {
+		display: flex;
+		gap: 12px;
+	}
+	.social {
+		display: flex;
 		justify-content: center;
 		align-items: center;
-		gap: 10px;
+		padding: 20px;
+		border: 2px solid #E7E7E7;
+		border-radius: 100px;	
 	}
-	.right-links {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-		align-items: center;
-		gap: 27px;
+	.social:hover {
+		border-color: var(--primary-color);
 	}
-	
-	.right-links a{
-		color: var(--text-secondary-color);
-	}
-	.right-links a:hover{
-		color: var(--primary-color);
-	}
-
-	.button-header {
-		background-color: var(--secondary-button-color);
-		height: initial;
-		max-height: 28px;
-	}
-
-	.button-header:hover {
+	.new-courses-button {
 		background-color: var(--primary-color);
 		color: white;
+		padding: 18px 30px 18px 30px;
+		border-radius: 50px;	
+		margin-left: 25px;
 	}
-
-	.button-secondary {
-		border: 1px solid var(--text-tertiary-color);
-		color: var(--text-secondary-color);
-		display: flex;
-		align-items: center;
-		padding: 0 10px 0 0;
+	.new-courses-button img {
+		width: 20px;
 	}
-	.button-secondary span {
-		padding: 0 10px 0 20px;
-	}
-
-	a:hover {
+	.new-courses-button:hover {
 		text-decoration: none;
 	}
 </style>
