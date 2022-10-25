79a55c810861007a4042e6cc7c158fd584c318d4
Ural <ural2001@gmail.com>
2022-10-25 16:47:15 +0300

    make course page

--- src/routes/courses/[slug]/+page.svelte
+++ src/routes/courses/[slug]/+page.svelte
@@ -2,7 +2,7 @@
     import ButtonArrowLong from '$lib/components/button/ButtonArrowLong.svelte';
 </script>
 
-<div class="container">
+<div class="container course">
     <div class="course-info">
         <div class="breadcrumbs">
             <a href="/">баш</a>
@@ -80,17 +80,343 @@
         </div>
     </div>
     <div class="course-content">
+        <img class="course-img" src="/img/courses/big.png" alt="">
+        <div class="course-content-block">
+            <p class="course-content-title">курсның эчтәлеге</p>
+            <div class="course-duration">
+                <span>14 дәрес · 3 сәг 40 мин</span>
+            </div>
+        </div>
+        <div class="modules">
+            <div class="module">
+                <span class="module-title">Беренче модуль</span>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Башлангыч сыйныфларда татар телен укытуда уеннар һә...
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Балаларны дәрескә һәм предметка ничек җәлеп итәргә?
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Чит илләрдә онлайн мәктәптә укучыларны татар теленә...
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Татар телен укытуда заманча дигитал ресурслар
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+            </div>
+            <div class="module">
+                <span class="module-title">Икенче модуль</span>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Башлангыч сыйныфларда татар телен укытуда уеннар һә...
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Балаларны дәрескә һәм предметка ничек җәлеп итәргә?
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+            </div>
+            <div class="module">
+                <span class="module-title">Өченче модуль</span>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Башлангыч сыйныфларда татар телен укытуда уеннар һә...
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Балаларны дәрескә һәм предметка ничек җәлеп итәргә?
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Чит илләрдә онлайн мәктәптә укучыларны татар теленә...
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+                <hr>
+                <a href="" class="module-lesson">
+                    <div>
+                        <img src="/icons/Note.svg" alt="">
+                        <span class="lesson-name">
+                            Татар телен укытуда заманча дигитал ресурслар
+                        </span>
+                    </div>
+                    <div class="lesson-duration">
+                        <img src="/icons/Clock.svg" alt="">
+                        <span class="lesson-duration-text">15 мин</span>
+                    </div>
+                </a>
+            </div>
+            <a href="" class="module modules-link">
+                <img src="/icons/NotePencilPurple.svg" alt="">
+                <span class="module-title">Биремнәр мисаллар карау</span>
+            </a>
+            <a href="" class="module modules-link">
+                <img src="/icons/MedalPurple.svg" alt="">
+                <span class="module-title">Дипломнар, сертификатлар һәм грамоталар мисаллары</span>
+            </a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="reviews-nav">
+        <h3>курслар турында фикерләр</h3>
+        <div class="reviews-nav-buttons">
+            <div class="reviews-nav-button-left">
+                <img src="/icons/CaretLeft.svg" alt="">
+            </div>
+            <div class="reviews-nav-button-right">
+                <img src="/icons/CaretRight.svg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="reels">
+        <div class="reel">
+            <img class="reel-img" src="/img/review-reels/1.png" alt=""> 
+            <img class="gradient" src="/img/review-reels/gradient.png" alt="">
+            <div class="reel-texts">
+                <a href="" class="reel-play-button">
+                    <img src="/icons/Play.svg" alt="">
+                </a>
+                <div class="reel-info">
+                    <span class="desc">6 курс узды</span>
+                    <span class="name">Талия, 17</span>
+                </div>
+            </div>
+        </div>
+        <div class="reel">
+            <img class="reel-img" src="/img/review-reels/1.png" alt=""> 
+            <img class="gradient" src="/img/review-reels/gradient.png" alt="">
+            <div class="reel-texts">
+                <a href="" class="reel-play-button">
+                    <img src="/icons/Play.svg" alt="">
+                </a>
+                <div class="reel-info">
+                    <span class="desc">6 курс узды</span>
+                    <span class="name">Талия, 17</span>
+                </div>
+            </div>
+        </div>
+        <div class="reel">
+            <img class="reel-img" src="/img/review-reels/1.png" alt=""> 
+            <img class="gradient" src="/img/review-reels/gradient.png" alt="">
+            <div class="reel-texts">
+                <a href="" class="reel-play-button">
+                    <img src="/icons/Play.svg" alt="">
+                </a>
+                <div class="reel-info">
+                    <span class="desc">6 курс узды</span>
+                    <span class="name">Талия, 17</span>
+                </div>
+            </div>
+        </div>
+         <div class="reel">
+            <img class="reel-img" src="/img/review-reels/1.png" alt=""> 
+            <img class="gradient" src="/img/review-reels/gradient.png" alt="">
+            <div class="reel-texts">
+                <a href="" class="reel-play-button">
+                    <img src="/icons/Play.svg" alt="">
+                </a>
+                <div class="reel-info">
+                    <span class="desc">6 курс узды</span>
+                    <span class="name">Талия, 17</span>
+                </div>
+            </div>
+        </div>
+        <div class="reel">
+            <img class="reel-img" src="/img/review-reels/1.png" alt=""> 
+            <img class="gradient" src="/img/review-reels/gradient.png" alt="">
+            <div class="reel-texts">
+                <a href="" class="reel-play-button">
+                    <img src="/icons/Play.svg" alt="">
+                </a>
+                <div class="reel-info">
+                    <span class="desc">6 курс узды</span>
+                    <span class="name">Талия, 17</span>
+                </div>
+            </div>
+        </div>
+        <div class="reel">
+            <img class="reel-img" src="/img/review-reels/1.png" alt=""> 
+            <img class="gradient" src="/img/review-reels/gradient.png" alt="">
+            <div class="reel-texts">
+                <a href="" class="reel-play-button">
+                    <img src="/icons/Play.svg" alt="">
+                </a>
+                <div class="reel-info">
+                    <span class="desc">6 курс узды</span>
+                    <span class="name">Талия, 17</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="courses-foryou-section container">
+    <div class="courses-foryou-nav">
+        <h3>сезнең өчен курслар</h3>
+        <div class="courses-nav-buttons">
+            <div class="courses-nav-button-left">
+                <img src="/icons/CaretLeft.svg" alt="">
+            </div>
+            <div class="courses-nav-button-right">
+                <img src="/icons/CaretRight.svg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="courses-foryou">
+        <div class="course-foryou-card">
+            <div>
+                <img src="/img/courses/course3.png" alt="">
+                <a href="" class="course-foryou-card-button">
+                    <img src="/icons/ArrowUpRight.svg" alt="">
+                </a>
+            </div>
+            <div class="course-foryou-card-right">
+                <p class="course-foryou-card-title">Татар әдәбияты тарихы: яңа заман әдәбияты</p>
+                <div class="course-foryou-card-info">
+                    <div>
+                        <span>Әдәбият</span>
+                    </div>
+                    <div>
+                        <span>12 дәрес</span>
+                    </div>
+                </div>
+            </div>
+		</div>
+        <div class="course-foryou-card">
+            <div>
+                <img src="/img/courses/course1.png" alt="">
+                <a href="" class="course-foryou-card-button">
+                    <img src="/icons/ArrowUpRight.svg" alt="">
+                </a>
+            </div>
+            <div class="course-foryou-card-right">
+                <p class="course-foryou-card-title">Татар әдәбияты тарихы: яңа заман әдәбияты</p>
+                <div class="course-foryou-card-info">
+                    <div>
+                        <span>Әдәбият</span>
+                    </div>
+                    <div>
+                        <span>12 дәрес</span>
+                    </div>
+                </div>
+            </div>
+		</div>
+        <div class="course-foryou-card">
+            <div>
+                <img src="/img/courses/course2.png" alt="">
+                <a href="" class="course-foryou-card-button">
+                    <img src="/icons/ArrowUpRight.svg" alt="">
+                </a>
+            </div>
+            <div class="course-foryou-card-right">
+                <p class="course-foryou-card-title">Татар әдәбияты тарихы: яңа заман әдәбияты</p>
+                <div class="course-foryou-card-info">
+                    <div>
+                        <span>Әдәбият</span>
+                    </div>
+                    <div>
+                        <span>12 дәрес</span>
+                    </div>
+                </div>
+            </div>
+		</div>
     </div>
 </div>
 
 <style>
+    .course {
+        display: flex;
+        justify-content: space-between;
+        gap: 80px;
+    }
     .page-title{
 		font-size: 52px;
         max-width: 540px;
         margin-bottom: 35px;
 	}
     .course-info {
-        width: 41%;
+        /* width: 41%; */
     }
     .actions {
         display: flex;
@@ -184,5 +510,220 @@
     .teacher-occupation {
         font-size: 14px;
         color: var(--primary-color);
+    }
+    .course-content {
+        /* width: 50%; */
+    }
+    .course-content .course-img{
+        margin-bottom: 100px;
+    }
+    .course-content-block {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .course-content-title {
+        font-size: 32px;
+        font-weight: 600;
+        margin-bottom: 0;
+    }
+    .course-duration {
+        background-color: #1A233E;
+        color: white;
+        padding: 3px 20px;
+        border-radius: 50px;
+        font-size: 12px;
+    }
+    .modules {
+        margin-top: 25px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+    .module {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 30px 40px;
+        border: 1px solid #E7E7E7;
+        border-radius: 24px;
+    }
+    .module-title {
+        font-weight: 600;
+        font-size: 22px;
+        color: var(--primary-color); 
+    }
+    .module-lesson {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 15px;
+    }
+    .module-lesson:hover {
+        text-decoration: none;
+        color: var(--primary-color);
+    }
+    .module hr {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    .lesson-name {
+        margin-left: 10px;
+        font-size: 16px;
+        font-weight: 500;
+    }
+    .lesson-duration {
+        display: flex;
+        font-size: 12px;
+        color: #999999;
+    }
+    .modules-link {
+        flex-direction: row;
+        justify-content: flex-start;
+        gap: 15px;
+        padding: 15px 40px;
+    }
+    .modules-link span {
+        font-size: 16px;
+        font-weight: 500;
+    }
+    .modules-link img {
+        width: 24px;
+    }
+
+    .courses-foryou-nav,
+    .reviews-nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 140px;
+        margin-bottom: 40px;
+    }
+    .courses-foryou-nav h3,
+    .reviews-nav h3 {
+        font-size: 40px;
+    }
+    .courses-nav-buttons,
+    .reviews-nav-buttons {
+		display: flex;
+	}
+	.courses-nav-button-left, .courses-nav-button-right,
+    .reviews-nav-button-left, .reviews-nav-button-right {
+		/* display: inline-block; */
+		/* margin-top: 100px; */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1px solid #E7E7E7;
+		border-radius: 150px;
+		padding: 18px;
+	}
+	.courses-nav-button-right,
+    .reviews-nav-button-right {
+		border: 1px solid var(--primary-color);
+		margin-left: -10px;
+	}
+	.courses-nav-button-left:hover, .courses-nav-button-right:hover,
+    .reviews-nav-button-left:hover, .reviews-nav-button-right:hover {
+		cursor: pointer;
+	}
+    .courses-foryou {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 5px;
+    }
+    .course-foryou-card {
+		display: flex;
+	}
+    .course-foryou-card img{
+		width: 172px;
+        z-index: -1;
+	}
+	.course-foryou-card-button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 48px;
+		height: 48px;
+		border: 1px solid var(--primary-color);
+		border-radius: 50px;
+        margin-top: -106px;
+        margin-left: 150px;
+	}
+	.course-foryou-card-button img {
+		width: 24px;
+	}
+    .course-foryou-card-right {
+        margin-left: 15px;
+    }
+	.course-foryou-card-title {
+		font-size: 20px;
+	}
+	.course-foryou-card-info {
+		display: flex;
+		gap: 8px;
+	}
+	.course-foryou-card-info div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 0px 15px;
+		border: 1px solid #999999;
+		border-radius: 50px;
+		color: #999999;
+		font-size: 12px;
+	}
+	.course-foryou-card-info span {
+		margin-top: -2px;
+    }
+    .reels {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+        gap: 15px;
+    }
+    .reel {
+        border-radius: 24px;
+    }
+    .reel-img {
+
+    }
+    .gradient {
+        position: absolute;
+        margin-left: -203px;
+        z-index: -1;
+    }
+    .reel-texts {
+        display: flex;
+        flex-direction: column;
+        margin-top: -158px;
+        margin-left: 20px;
+    }
+    .reel-play-button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+        border: 1px solid white;
+        border-radius: 100px;
+        width: 72px;
+        height: 72px;
+        margin-bottom: 15px;
+    }
+    .reel-play-button img {
+        margin-left: 5px;
+    }
+    .reel-info {
+        display: flex;
+        flex-direction: column;
+    }
+    .reel .desc {
+        color: white;
+        opacity: 0.6;
+        font-size: 12px;
+        font-weight: 500;
+    }
+    .reel .name {
+        color: white;
+        font-weight: 600;
     }
 </style>
